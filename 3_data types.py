9126017828a1481715ddb701490f576c964ba855
Gnanas458 <gnanas458@gmail.com>
2025-02-06 20:18:46 +0530

    Add files via upload

---
+++ 3_data types.py
@@ -0,0 +1,15 @@
+x = 5
+y = 5.5
+z = "abc"
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+l = []
+t =("a","b","c")
+d = {"a" :2, "b":2, "c":5}
+
+print(type(l))
+print(type(t))
+print(type(d))
